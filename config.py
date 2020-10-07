5ba164f7d6b9969f8dd64a9b75b4c08b7193e99a
Val Knight <val@valknight.xyz>
2020-10-07 15:59:31 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,31 @@
+# Change this if you are not debugging on a robot, but instead using a local client to test features
+# Note: this is not recommended for most development, as micropython is used instead of standard python
+# Avoid if possible.
+running_on_robot = True
+# disable this if you are running on robot but want to disable the actual movement part
+do_movement = True
+# Debug mode disables clearing and will print raw json received, and any errors will be shown
+debug = False
+# The following import is used 
+if running_on_robot:
+    from ev3dev2.motor import list_motors, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, MoveJoystick
+
+if running_on_robot:
+    # Change output_B and C respectively if you wire your wheels differently
+    left_wheel, right_wheel = OUTPUT_B, OUTPUT_C
+    arm_port = OUTPUT_A
+    # Config
+    # disable if your robot does not have an arm
+    arm_enabled = True
+    # configure which output is the arm
+    arm_position_dxown = 10
+    arm_position_up = 0
+    turn_speed_multiplier = 0.5
+    forward_turn_multiplier = 1
+    # Change if you want your brick to go slower or faster
+    # Around 30-50 is best for a EV3 connected to a server over bluetooth
+    speed = 40
+    # Get font list from https://ev3dev-lang.readthedocs.io/projects/python-ev3dev/en/stable/console.html#console-fonts
+    font = "Lat15-VGA8.psf.gz"
+    # The following means we    
+    drive = MoveJoystick(left_wheel, right_wheel)
---
+++ main.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env micropython
+from time import sleep, time
+import json
+from config import *
+if running_on_robot:
+    from urllib.urequest import urlopen
+    from urequests import request
+    from ev3dev2.console import Console
+    from ev3dev2.motor import list_motors, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, MoveJoystick
+    console = Console()
+    console.set_font(font)
+else:
+    from urllib.request import urlopen
+
+
+def get_robot_data():
+    try:
+        with open('../robot.json', 'r') as f:
+            return json.loads(f.read())
+    except FileNotFoundError:
+        pass
+    try:
+        with open('robot.json', 'r') as f:
+            return json.loads(f.read())
+    except FileNotFoundError:
+        raise FileNotFoundError("Could not find file in either current directory or higher level directory")
+
+
+data = get_robot_data()
+robotName = data['name']
+robotId = data['id']
+robotKey = data['key']
+host = data['host']
+
+postData = json.dumps({
+    "robotId": robotId,
+    "robotName": robotName,
+    "robotKey": robotKey})
+
+buttons = []
+oldest_time = "/"
+
+def get_commands():
+    # TODO: fix this for local debugging purposes
+    # TODO: revert to old urequests way of getting data - it was more reliable, and the latency difference isn't noticiable
+    s = request("POST", "{}/robot".format(host), json=postData)
+    return s.json()
+
+
+
+def refresh_display(buttons):
+    # Manage screen
+    if not debug:
+        console.reset_console()
+    global oldest_time
+    print("Running")
+    print("ID {}".format(robotId))
+    print("Name: {}".format(robotName))
+    print("Latency: {}ms".format(oldest_time))
+    # Handle latency calculation
+    if len(buttons) > 0:
+        oldest_time = buttons[0]['time']
+        for button in buttons:
+            if button['time'] < oldest_time:
+                oldest_time = button['time']
+        oldest_time = time() * 1000 - oldest_time
+
+def handle_movement(buttons):
+    x, z = buttons['coords'][0], buttons['coords'][2]
+    z *= speed
+    x *= (speed * turn_speed_multiplier)
+    if z:
+        x *= forward_turn_multiplier
+    if do_movement:
+        drive.on(x, z)
+
+
+while True:
+    try:
+        j = get_commands()
+        buttons = j['buttons']
+        refresh_display(buttons)
+        handle_movement(j)
+    except Exception as e:
+        if debug:
+            print(e)
+        pass
+    sleep(0.01)
