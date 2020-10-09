ee2176d65ac31b57082cc78a34b7628fdb438052
Val Knight <val@valknight.xyz>
2020-10-09 18:09:06 +0100

    Make config tied to robot be things which HAVE to have ports
    Signed-off-by: Val Knight <val@valknight.xyz>

--- config.py
+++ config.py
@@ -6,6 +6,18 @@ running_on_robot = True
 do_movement = True
 # Debug mode disables clearing and will print raw json received, and any errors will be shown
 debug = False
+# disable if your robot does not have an arm
+arm_enabled = True
+# configure which output is the arm
+arm_position_dxown = 10
+arm_position_up = 0
+turn_speed_multiplier = 0.5
+forward_turn_multiplier = 1
+# Get font list from https://ev3dev-lang.readthedocs.io/projects/python-ev3dev/en/stable/console.html#console-fonts
+font = "Lat15-VGA8.psf.gz"
+# Change if you want your brick to go slower or faster
+# Around 30-50 is best for a EV3 connected to a server over bluetooth
+speed = 40
 # The following import is used 
 if running_on_robot:
     from ev3dev2.motor import list_motors, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, MoveJoystick
@@ -14,18 +26,5 @@ if running_on_robot:
     # Change output_B and C respectively if you wire your wheels differently
     left_wheel, right_wheel = OUTPUT_B, OUTPUT_C
     arm_port = OUTPUT_A
-    # Config
-    # disable if your robot does not have an arm
-    arm_enabled = True
-    # configure which output is the arm
-    arm_position_dxown = 10
-    arm_position_up = 0
-    turn_speed_multiplier = 0.5
-    forward_turn_multiplier = 1
-    # Change if you want your brick to go slower or faster
-    # Around 30-50 is best for a EV3 connected to a server over bluetooth
-    speed = 40
-    # Get font list from https://ev3dev-lang.readthedocs.io/projects/python-ev3dev/en/stable/console.html#console-fonts
-    font = "Lat15-VGA8.psf.gz"
     # The following means we    
     drive = MoveJoystick(left_wheel, right_wheel)
